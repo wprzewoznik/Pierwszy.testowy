a8d7ece7fb71190add1c6c2b13a7c20ed3e874f5
wprzewoznik <w.przewoznik@gmail.com>
2020-10-25 16:09:03 +0100

    Poprawa - funkcja juz dziala

--- R/fun_pack.R
+++ R/fun_pack.R
@@ -1,11 +1,11 @@
 fun_pack <- function(a = 10, b = 20, d = 2) {
   out <- sqrt(a*b*d/2 + 100 + 1)
 
-  out <- stats::rnorm(n = base::round(out, 0)) %>%
+  out <- stats::rnorm(n = base::round(out, 0))
   out <- base::as.data.frame(out)
 
   out <- dplyr::summarise(out,
-                          mean=base::mean(., na.rm = T))
+                          mean=base::mean(out, na.rm = T))
 
 
   return(out)
