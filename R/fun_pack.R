93631f89e47dc6be04eaaab8e43d3aa4794ee406
wprzewoznik <w.przewoznik@gmail.com>
2020-10-25 16:03:33 +0100

    Poprawy w temacie uzywanych funkcji

--- R/fun_pack.R
+++ R/fun_pack.R
@@ -1,11 +1,11 @@
 fun_pack <- function(a = 10, b = 20, d = 2) {
   out <- sqrt(a*b*d/2 + 100 + 1)
 
-  out <- rnorm(n = round(out, 0)) %>%
-    as.data.frame()
+  out <- stats::rnorm(n = base::round(out, 0)) %>%
+  out <- base::as.data.frame(out)
 
-  out <- out %>%
-    summarise(mean=mean(., na.rm = T))
+  out <- dplyr::summarise(out,
+                          mean=base::mean(., na.rm = T))
 
 
   return(out)
